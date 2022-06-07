5d4ab00e8848d1a5b8e7439f7f44d616e256843e
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-08 00:55:59 +0200

    {small screen edited}

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,5 @@
 import "./home.css";
-
+import {CgQr} from 'react-icons/cg'
 
 
 export default function Home(){
@@ -20,11 +20,23 @@ export default function Home(){
                 </div>
         </div>
         <div className="homeContentRight">
+            <div className="centeredContent">
             <h1>Rice Cream</h1>
             <p>
                 Rice Extract Moisturing Cream with Organic Evening Primrose, Rice Bran Oil, <br></br>Shea Butter and Rice Callus Extract 80g.
             </p>
+            <div className="priceContent">
             <h3>$19,00</h3>
+            <span><i><CgQr/></i><small>Add discount code</small></span>
+            </div>
+            <div className="detailContent">
+                <ul>
+                    <li><span>Details</span></li>
+                    <li><span>Ingredients</span></li>
+                    <li><span>Direction</span></li>
+                </ul>
+            </div>
+            </div>
         <div className="homeContentRightTwo">
         </div> 
         </div>  
