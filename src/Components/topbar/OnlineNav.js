79292acb7f5d80f92cc503234f27d14df36fa901
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-07 13:00:59 +0200

    {icon added}

--- src/Components/topbar/OnlineNav.js
+++ src/Components/topbar/OnlineNav.js
@@ -4,18 +4,30 @@ import './topbar.css'
 /*Online Nav Bar*/
 //Topbar Icons
 import {NavLink} from 'react-router-dom'
-import {CgHome} from "react-icons/cg";
+import {CgArrowLeft} from 'react-icons/cg';
+import {CgArrowRight} from 'react-icons/cg';
+import {CgSearch} from 'react-icons/cg';
+import {CgShoppingBag} from 'react-icons/cg';
+
+
+
 function OnlineNav(props) {
     //Topbar navlinks Online
     return (
         <div>
             <div className="linksContainer">  
-                    <NavLink to="/Home" activeStyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span><i><CgHome/>Home</i></span>
-                    </NavLink>
-                    <div id="horizontalScroll">
-            <span><i class="fas fa-arrow-left" onclick="left()" id="scrollLeft"></i></span>
-            <span><i class="fas fa-arrow-right" onclick="right()" id="scrollRight" ></i></span>
-         </div>
+                <NavLink to="/Home" activeStyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span><button><i> <CgArrowLeft/> </i></button></span>
+                </NavLink>
+                <NavLink to="/Home" activeStyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span><button><i ><CgArrowRight/></i></button></span>
+                </NavLink>
+                <NavLink to="/Home" activeStyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span><div><input type="search" placeholder="Search"></input><i><CgSearch/></i></div></span>
+                </NavLink>
+                <NavLink to="/Home" activeStyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span><div><i ><CgShoppingBag/></i></div></span>
+                </NavLink>
+                <NavLink to="/Home" activeStyle={{color: 'black'}} exact onClick={() => props.isMobile && props.closeMobileMenu()}><span><div><i class="bi bi-grid"></i></div></span>
+                </NavLink>
+            
+                   
             </div>
         </div>
     )
