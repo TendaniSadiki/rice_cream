41a8b2a2a2f27943fa3e40f5fa88b6c28a04c051
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-07 16:02:33 +0200

    {content added}

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,9 +11,16 @@ export default function Home(){
         <br></br>
         <br></br>
         <br></br>
-            <div className="leftContent">
-
-            </div>
+        <div className='leftContent'>
+                 
+        </div>
+        <div className="contentOver">
+                <h1>over</h1>
+        </div>
+        <div className="homeContentRight">
+        </div>  
+        <div className="homeContentRightTwo">
+        </div> 
             
            
         </div>
