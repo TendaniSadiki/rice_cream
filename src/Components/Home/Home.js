e8b4296580bce2f3951763b405c824483ee40855
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-08 02:44:47 +0200

    {right content edited}

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,8 @@
 import "./home.css";
-import {CgQr} from 'react-icons/cg'
+import {CgQr} from 'react-icons/cg';
+import {CgBookmark} from 'react-icons/cg';
+import {CgCamera} from 'react-icons/cg';
+import {CgAdd} from 'react-icons/cg';
 
 
 export default function Home(){
@@ -36,8 +39,24 @@ export default function Home(){
                     <li><span>Direction</span></li>
                 </ul>
             </div>
+            <div className="bottomContent">
+                <div className="leftBottom">
+                <h1>I'm left</h1>
+                </div>
+                <div className="rightBottom">
+                    <h1>I'm right</h1>
+                </div>
+            </div>
             </div>
         <div className="homeContentRightTwo">
+            <ul>
+                <li className="rightItems"><span><i><CgBookmark/></i><p>Save</p></span></li>
+                <li className="rightItems"><span><i><CgCamera/></i><p>Intro</p></span></li>
+                <li className="rightItems"><span><i><CgAdd/></i><p>Purchase</p></span></li>
+            </ul>
+            <ul>
+                <li className="overRall"></li>
+            </ul>
         </div> 
         </div>  
         
